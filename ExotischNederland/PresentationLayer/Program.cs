a2c1fc9b00aa66e2567d07f323e7ad5c2de226f8
Rick Vincken <2403211vincken@zuyd.nl>
2025-01-16 21:28:38 +0100

    Program.cs toevoeging: comment bovenaan met projectinformatie

--- ExotischNederland/PresentationLayer/Program.cs
+++ ExotischNederland/PresentationLayer/Program.cs
@@ -1,4 +1,16 @@
-﻿namespace ExotischNederland.PresentationLayer;
+﻿/*
+    Project name: Exotisch Nederland Casus
+    Project group: Arch BTW
+    Project members: Rick Vincken, Daan Ros, D'vaughn Dassen, Sylas Barendse | Klas B1D
+
+    Created: 01/16/2025
+
+    Purpose: Het programma in de vorm van een console applicatie dient als grof prototype
+             voor een applicatie voor het documenteren van waarnemingen van dier- en
+             plantsoorten in het wild zoals gevraagd door de opdrachtgever.
+*/
+
+namespace ExotischNederland.PresentationLayer;
 
 internal class Program
 {
