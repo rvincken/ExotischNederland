5b29203de2c19432ebc3ef0964e7d24ab35e14c2
Daan Ros <2402709ros@zuyd.nl>
2025-01-16 13:04:28 +0100

    16-01-2025

--- ExotischNederland/Model/Locatie.cs
+++ ExotischNederland/Model/Locatie.cs
@@ -5,9 +5,43 @@
 
 internal class Locatie
 {
-    private int id;
-    private string locatienaam;
-    private string provincie;
-    private double breedtegraad;
-    private double lengtegraad;
+    private int _id;
+    private string _locatienaam;
+    private string _provincie;
+    private double _breedtegraad;
+    private double _lengtegraad;
+
+    public int Id
+    {
+        get { return _id; }
+    }
+
+    public string Locatienaam
+    {
+        get { return _locatienaam; }
+    }
+
+    public string Provincie
+    {
+        get { return _provincie; }
+    }
+
+    public double Breedtegraad
+    {
+        get { return _breedtegraad; }
+    }
+
+    public double Lengtegraad
+    {
+        get { return _lengtegraad; }
+    }
+
+    public Locatie(int id, string locatienaam, string provincie, double breedtegraad, double lengtegraad)
+    {
+        _id = id;
+        _locatienaam = locatienaam;
+        _provincie = provincie;
+        _breedtegraad = breedtegraad;
+        _lengtegraad = lengtegraad;
+    }
 }
