c92f5cb7ab24c917fb51b81e2e627c3eae5dcb9d
rvincken <2403211vincken@zuyd.nl>
2025-01-17 10:03:03 +0100

    Private setters toegevoegd, met uitgewerkte grens implementaties voor Id, Aantal, Omschrijving, Toelichting, Geslacht en ManierDelen

--- ExotischNederland/Model/Waarneming.cs
+++ ExotischNederland/Model/Waarneming.cs
@@ -19,54 +19,141 @@ internal class Waarneming
     public int Id
     {
         get { return _id; }
+        private set 
+        {
+            if (value.ToString().Length <= 7)
+            {
+                _id = value;
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"{this}.Id kan niet langer zijn dan 7 getallen, " +
+                    $"Maar een getal van {value} is gegeven.");
+            }
+        }
     }
     public int Aantal
     {
         get { return _aantal; }
+        private set
+        {
+            if (value.ToString().Length <= 7)
+            {
+                _aantal = value;
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"{this}.Aantal kan niet langer zijn dan 7 getallen, " +
+                    $"Maar een getal van {value} is gegeven.");
+            }
+        }
     }
     public string Omschrijving
     {
         get { return _omschrijving; }
+        private set
+        {
+            if (value.Length <= 200)
+            {
+                _omschrijving = value;
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"{this}.Omschrijving kan niet langer zijn dan 200 karakters, " +
+                    $"Maar een string van:\n{value}\n... is gegeven.");
+            }
+        }
     }
     public string Toelichting
     {
         get { return _toelichting; }
+        private set
+        {
+            if (value.Length <= 200)
+            {
+                _toelichting = value;
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"{this}.Toelichting kan niet langer zijn dan 200 karakters, " +
+                    $"Maar een string van:\n{value}\n... is gegeven.");
+            }
+        }
     }
     public string Datum
     {
         get { return _datum; }
+        private set { _datum = value; }
     }
     public string Tijd
     {
         get { return _tijd; }
+        private set { _tijd = value; }
     }
     public char Geslacht
     {
         get { return _geslacht; }
+        private set
+        {
+            value = Char.ToLower(value);
+            if (value == 'm' || value == 'f')
+            {
+                _geslacht = value;
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"{this}.Geslacht kan alleen 'm'/'f' zijn, " +
+                    $"maar een waarde van {value} is gegeven.");
+            }
+        }
     }
     public char ManierDelen
     {
         get { return _manierDelen; }
+        private set
+        {
+            value = Char.ToLower(value);
+            if (value == 'n' || value == 'v' || value == 'o')
+            {
+                _geslacht = value;
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"{this}.ManierDelen kan alleen 'n'/'v'/'o' zijn, " +
+                    $"maar een waarde van {value} is gegeven.");
+            }
+        }
     }
     public bool Zekerheid
     {
         get { return _zekerheid; }
+        private set { _zekerheid = value; }
     }
     public Gebruiker Waarnemer
     {
         get { return _waarnemer; }
+        private set { _waarnemer = value; }
     }
     public Foto Afbeelding
     {
         get { return _afbeelding; }
+        private set { _afbeelding = value; }
     }
     public Locatie WaarnemingLocatie
     {
         get { return _waarnemingLocatie; }
+        private set { _waarnemingLocatie = value; }
     }
     public Soort WaarnemingSoort
     {
         get { return _waarnemingSoort; }
+        private set { _waarnemingSoort = value; }
     }
 
     public Waarneming
@@ -75,19 +162,19 @@ internal class Waarneming
         bool zekerheid, Gebruiker waarnemer, Foto afbeelding,
         Locatie waarnemingLocatie, Soort waarnemingSoort)
     {
-        _id = id;
-        _aantal = aantal;
-        _omschrijving = omschrijving;
-        _toelichting = toelichting;
-        _datum = datum;
-        _tijd = tijd;
-        _geslacht = geslacht;
-        _manierDelen = manierDelen;
-        _zekerheid = zekerheid;
-        _waarnemer = waarnemer;
-        _afbeelding = afbeelding;
-        _waarnemingLocatie = waarnemingLocatie;
-        _waarnemingSoort = waarnemingSoort;
+        Id = id;
+        Aantal = aantal;
+        Omschrijving = omschrijving;
+        Toelichting = toelichting;
+        Datum = datum;
+        Tijd = tijd;
+        Geslacht = geslacht;
+        ManierDelen = manierDelen;
+        Zekerheid = zekerheid;
+        Waarnemer = waarnemer;
+        Afbeelding = afbeelding;
+        WaarnemingLocatie = waarnemingLocatie;
+        WaarnemingSoort = waarnemingSoort;
     }
 
     public override string ToString()
@@ -95,7 +182,7 @@ internal class Waarneming
         // Verbeter later:
         // hoe waarnemer, afbeelding, waarnemingLocatie en waarnemingSoort getoond worden
         string manierVanDelen;
-        switch (_manierDelen)
+        switch (ManierDelen)
         {
             case 'n':
                 manierVanDelen = "Niet delen";
@@ -112,16 +199,16 @@ internal class Waarneming
         }
 
         return $"""
-                ID: {_id}
-                Aantal: {_aantal}
-                Omschrijving: {_omschrijving}
-                Toelichting: {_toelichting}
-                Datum: {_datum}
-                Tijd: {_tijd}
+                ID: {Id}
+                Aantal: {Aantal}
+                Omschrijving: {Omschrijving}
+                Toelichting: {Toelichting}
+                Datum: {Datum}
+                Tijd: {Tijd}
                 Manier van delen: {manierVanDelen}
-                Geslacht: {(_geslacht == 'm' ? "Male" : "Female")}
-                Zekerheid: {(_zekerheid ? "Zeker" : "Onzeker")}
-                Afbeelding toegevoegd: {(_afbeelding != null ? "Ja" : "Nee")}
+                Geslacht: {(Geslacht == 'm' ? "Male" : "Female")}
+                Zekerheid: {(Zekerheid ? "Zeker" : "Onzeker")}
+                Afbeelding toegevoegd: {(Afbeelding != null ? "Ja" : "Nee")}
                 """;
     }
 }
