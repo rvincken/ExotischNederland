15718356df9ca7d0d33e9bf4d0d5b47a921a96db
dvaughn-dassen <2406537dassen@zuyd.nl>
2025-01-17 11:10:22 +0100

    ik heb get en set toegevoegd en ik heb een constructor gemaakt.

--- ExotischNederland/Model/Soort.cs
+++ ExotischNederland/Model/Soort.cs
@@ -5,4 +5,86 @@
 
 internal class Soort
 {
+    private string _wetenschappelijkeNaam;
+    private string _soortNaam;
+    private int _hoeVaakDeSoortVoorkomt;
+    private char _oorsprong;
+
+    public string WetenschappelijkeNaam
+    {
+        get { return _wetenschappelijkeNaam; }
+        set
+        {
+            if (value.Length <=50)
+            {
+                _wetenschappelijkeNaam = value;
+             
+            }
+            else
+            {
+                throw new ArgumentException("wetenschappelijke naam is niet bekend");
+            }
+        }
+    }
+
+    public string SoortNaam
+    {
+        get { return _soortNaam; }
+        set
+        {
+            if (value.Length <= 50)
+            {
+                _soortNaam = value;
+            }
+            else
+            {
+                throw new ArgumentException("soort naam is niet bekend");
+            }
+        }
+    }
+
+    public int HoeVaakDeSoortVoorkomt
+    {
+        get { return _hoeVaakDeSoortVoorkomt; }
+        set
+        {
+            if (value <= 50)
+            {
+                _hoeVaakDeSoortVoorkomt = value;
+            }
+            else
+            {
+                throw new ArgumentException("de soort komt niet vaak voor");
+            }
+        }
+    }
+
+    public char Oorsprong
+    {
+        get { return _oorsprong; }
+        set
+        {
+            if (value <= 50)
+            {
+                _oorsprong = value;
+            }
+            else
+            {
+                throw new ArgumentException("oorsprong is niet bekend");
+            }
+
+
+        }
+    }
+    public Soort(string wetenschappelijkeNaam, string soortNaam, int hoeVaakDeSoortVoorkomt, char oorsprong )
+    {
+        WetenschappelijkeNaam = wetenschappelijkeNaam;
+        SoortNaam = soortNaam;
+        HoeVaakDeSoortVoorkomt = hoeVaakDeSoortVoorkomt ;
+        Oorsprong = oorsprong;
+        
+    }
 }
+
+
+
