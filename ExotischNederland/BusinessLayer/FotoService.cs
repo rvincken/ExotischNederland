aca360c8b2a039b23dd3514c0165db41f6e9a1f1
Rick Vincken <2403211vincken@zuyd.nl>
2025-01-19 19:13:13 +0100

    Implementatie FotoService klasse

---
+++ ExotischNederland/BusinessLayer/FotoService.cs
@@ -0,0 +1,23 @@
+﻿namespace ExotischNederland.BusinessLayer;
+
+internal class FotoService
+{
+    private readonly DataLayer.FotoRepository _repository;
+
+    public FotoService()
+    {
+        _repository = new DataLayer.FotoRepository();
+    }
+
+    public void RegistreerFoto(int id, string afbeeldingPad)
+    {
+        var foto = new Model.Foto(id, afbeeldingPad);
+
+        _repository.VoegFotoToe(foto);
+    }
+
+    public List<Model.Foto> KrijgAlleFotos()
+    {
+        return _repository.HaalAlleFotosOp();
+    }
+}
