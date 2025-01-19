33113237e3231735b3a107de1bd3f5f9d9b5643f
Rick Vincken <2403211vincken@zuyd.nl>
2025-01-19 18:54:44 +0100

    Microsoft.Data.Sqlite dependency toegevoegd en FotoRepository geimplementeerd

---
+++ ExotischNederland/DataLayer/FotoRepository.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace ExotischNederland.DataLayer;
+
+internal class FotoRepository
+{
+    private readonly string _connectionString = "../Database/ExotischNederland.db";
+
+    public FotoRepository()
+    {
+        InitializeDatabase();
+    }
+
+    private void InitializeDatabase()
+    {
+        using var connection = new SqliteConnection(_connectionString);
+    }
+
+    public void VoegFotoToe(Model.Foto foto)
+    {
+        using var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+
+        string insertQuery = @"
+            INSERT INTO Foto (Fid, Afbeelding)
+            VALUES (@Fid, @Afbeelding);";
+
+        using var command = new SqliteCommand(insertQuery, connection);
+        command.Parameters.AddWithValue("@Fid", foto.Id);
+        command.Parameters.AddWithValue("@Afbeelding", foto.Afbeelding);
+
+        command.ExecuteNonQuery();
+    }
+
+    public List<Model.Foto> HaalAlleFotosOp()
+    {
+        var fotos = new List<Model.Foto>();
+
+        using var connection = new SqliteConnection(_connectionString);
+        connection.Open();
+
+        string selectQuery = @"
+            SELECT * FROM Foto;";
+
+        using var command = new SqliteCommand(selectQuery, connection);
+
+        using var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);
+            byte[] imageBytes = (byte[])reader["Afbeelding"];
+
+            fotos.Add(new Model.Foto(id, imageBytes));
+        }
+
+        return fotos;
+    }
+}
