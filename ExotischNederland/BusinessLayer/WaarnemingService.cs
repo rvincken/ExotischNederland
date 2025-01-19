6e1c7330bbdbc4149e357ee9c6647401933fbdb5
Rick Vincken <2403211vincken@zuyd.nl>
2025-01-19 19:44:59 +0100

    Implementatie van klasse WaarnemingService

---
+++ ExotischNederland/BusinessLayer/WaarnemingService.cs
@@ -0,0 +1,29 @@
+﻿namespace ExotischNederland.BusinessLayer;
+
+internal class WaarnemingService
+{
+    private readonly DataLayer.WaarnemingRepository _repository;
+
+    public WaarnemingService()
+    {
+        _repository = new DataLayer.WaarnemingRepository();
+    }
+
+    public void RegistreerWaarneming
+        (int id, int waarnemerId, int fotoId, int locatieId, int soortId,
+        string omschrijving, string datum, string tijd)
+    {
+        var waarneming = new Model.Waarneming
+        (   
+            id, waarnemerId, fotoId, locatieId, soortId,
+            omschrijving, datum, tijd
+        );
+
+        _repository.VoegWaarnemingToe(waarneming);
+    }
+
+    public List<Model.Waarneming> KrijgAlleWaarnemingen()
+    {
+        return _repository.HaalAlleWaarnemingenOp();
+    }
+}
